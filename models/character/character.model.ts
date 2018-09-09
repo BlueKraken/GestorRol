c986559f805fd3d32c043a8c8895ea7cbcf6c9e8
BlueKraken <gonzalo.tajmuch@gmail.com>
2018-09-09 15:11:26 -0300

    refactor modelo de personaje e implementa fabrica de atributos

--- models/character/character.model.ts
+++ models/character/character.model.ts
@@ -1,6 +1,5 @@
 import { Inventory } from '../inventory/inventory.model';
-import { CharacterAttribute } from '../characterAttribute/characterAttribute.model';
-/** FixMe atributos no actualizados */
+import { NumeralAttribute } from '../characterAttributes/basemodels/numeralAttribute.model';
 /** 
 * Representa un personaje del juego, sea 
 * controlado por un jugador, o no. 
@@ -21,128 +20,23 @@ export class Character
   /** Nombre predefinido dado por su clase especial */
   private predefinedName: string;
   /** Nombre de su clase */
-  public ClassName: string;
+  public get ClassName(): string { return this.className; }
+  private className: string;
   /** Nombre de su raza */
-  public RaceName: string;
+  public get RaceName(): string { return this.raceName; }
+  private raceName: string;
   /** Nivel de conexión */
-  public ConnectionLevel: number;
-  /** Clase de prestigio/oficio */
-  public JobOrPrestige: string;
-  /** Vida total */
-  private totalHealthPoints: number;
-  /** Devuelve la vida total del personaje */
-  public get TotalHealthPoints() {
-    return this.totalHealthPoints;
-  }
-  /** 
-   * Actualiza la vida total del personaje y
-   * su vida actual proporcionalmente.
-   */
-  public set TotalHealthPoints(value: number) {
-    this.HealthPoints *= value / this.totalHealthPoints;
-    this.totalHealthPoints = value;
-  }
-  /** Vida actual */
-  public HealthPoints: number;
-  /** Vida total */
-  public TotalEnergy: number;
-  /** Aguante */
-  public Stamina: number;
-  //#region Atributos modificadores
-  /** Moral base */
-  public BaseMorale: CharacterAttribute;
-  /** Moral por subida de nivel */
-  public ConnectionMorale: CharacterAttribute;
-  /** Moral propia */
-  public get OwnMorale(): number {
-    return this.BaseMorale.Value 
-      + this.ConnectionMorale.Value;
+  public get ConnectionLevel(): string { return this.connectionLevel; };
+  private connectionLevel: string;
+  /** Clases de prestigio */
+  public get Prestige1(): string { return this.prestiges[0] || ''};
+  public get Prestige2(): string { return this.prestiges[1] || ''};
+  public prestiges: Array<string>;
+  /** Atributos del personaje */
+  public get CharacterAttributes() {
+    return this.characterAttributes;
   };
-  /** Suerte */
-  public Luck: CharacterAttribute;
-  /** Iniciativa tirada */
-  public RolledInitiative: CharacterAttribute;
-  /** Valor modificado de iniciativa */
-  public get ModifiedInitiative(): CharacterAttribute {
-    return CharacterAttribute.ComposeAttributes(
-      this.RolledInitiative,
-      this.IntelligenceTests,
-      this.CharismaEloquenceDiplomacy
-    );
-  };
-  //#endregion
-  //#region Características de clase
-  /** Rapidez de acciones */
-  public ActionsSpeed: number;
-  /** Casillas avanzadas */
-  public AdvanceBoxes: CharacterAttribute;
-  /** Opciones de coste de energía */
-  public EnergyOptionsCost: string;
-  //#endregion
-  //#region Tiros y pruebas de salvación
-  /** Fortaleza */
-  public Fortitude: CharacterAttribute;
-  /** Pruebas de fuerza */
-  public StrengthTests: CharacterAttribute;
-  /** 
-   * Reflejos TODO: a reflejos en manos, cabeza, y torso y brazos deben
-   * sumarles la bonificación de reflejos
-  */
-  public Reflexes: CharacterAttribute;
-  public HeadReflexes: CharacterAttribute;
-  public HandsReflexes: CharacterAttribute;
-  public TorsoAndArmsReflexes: CharacterAttribute;
-  /** Pruebas de destreza */
-  public DexterityTests: CharacterAttribute;
-  /** Voluntad */
-  public Will: CharacterAttribute;
-  /** Pruebas de inteligencia */
-  public IntelligenceTests: CharacterAttribute;
-  //#endregion
-  //#region Habilidades sociales
-  /** Astucia y deducción */
-  public ClevernessAndDeduction: CharacterAttribute;
-  /** Conocimiento y experiencia */
-  public WisdomAndExperience: CharacterAttribute;
-  /** Carisma, elocuencia y diplomacia */
-  public CharismaEloquenceDiplomacy: CharacterAttribute;
-  //#endregion
-  //#region  Atributos secundarios
-  /** Promedio conocimiento/experiencia y pruebas de inteligencia */
-  private get wisdomIntelligenceAverage(): number {
-    const average = (this.WisdomAndExperience.Value 
-      + this.IntelligenceTests.Value) / 2;
-    return Math.round(average);
-  }
-  /** Detectar magia */
-  public get DetectMagic(): number {
-    return this.wisdomIntelligenceAverage;
-  }
-  /** Detectar inmunidades, fortalezas y debilidades */
-  public get DetectInmunitiesFortitudesAndWeakness(): number {
-    return this.wisdomIntelligenceAverage;
-  }
-  /** Descifrar escritura */
-  public get Deciphertext(): number {
-    return this.wisdomIntelligenceAverage;
-  }
-  /** Buscar */
-  public get Search(): number {
-    const average = (this.Luck.Value 
-      + this.ClevernessAndDeduction.Value) / 2;
-    return Math.round(average);
-  }
-  /** Sigilo */
-  public get Stealth(): number {
-    const average = (this.DexterityTests.Value 
-      + this.ClevernessAndDeduction.Value) / 2;
-    return Math.round(average);
-  }
-  /** Averiguar intenciones */
-  public get FindOutIntentions(): number {
-    return this.ClevernessAndDeduction.Value;
-  }
-  //#endregion 
+  private characterAttributes: Array<NumeralAttribute>; 
   /** Inventario */
   public Inventory: Inventory
   /** 
@@ -151,6 +45,7 @@ export class Character
    */
   constructor(name: string, characterClass: string, race: string) {
     //TODO: Lógica de creación
+
     throw 'Not implemented';
   }
 }
---
+++ models/characterAttributes/attributeFactory.ts
@@ -0,0 +1,20 @@
+import { CharacterAttributeName, CharacterAttribute } from './basemodels/characterAttribute.model'
+import { Stamina } from './stamina.model';
+import { DetectMagic } from './detectMagic.model';
+
+export function createAttribute(attributeName: CharacterAttributeName, initialValue: number): CharacterAttribute {
+  const constructor = constructorToNameMap.find(mi => mi[0] == attributeName)[1];
+
+  return new constructor(initialValue);
+}
+
+const constructorToNameMap: Array<DictionaryItem<CharacterAttributeName, any>> = [
+  ['stamina', Stamina],
+  ['detectMagic', DetectMagic],
+];
+
+interface DictionaryItem<K,V>  
+{ 
+  0: K, 
+  1: V 
+};
