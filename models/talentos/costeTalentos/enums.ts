e93301c1fb7520a576763ab44259a609cb20bb7e
BlueKraken <gonzalo.tajmuch@gmail.com>
2018-10-26 20:59:31 -0300

    agrega diccionario de naturaleza de coste

---
+++ models/talentos/costeTalentos/enums.ts
@@ -0,0 +1,9 @@
+export enum NaturalezaCoste {
+    Tiempo = 'Coste de Tiempo',
+    Energía = 'Coste de Energía',
+    Inversión = 'Coste de Inversión',
+    Pérdida = 'Coste de Pérdida',
+    Vida = 'Coste de Vida',
+    Aguante = 'Coste de aguante',
+    AtributoTiempo = 'Coste de Atributo/tiempo',
+}
